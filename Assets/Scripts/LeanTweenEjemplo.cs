cc9d2d7d57c2534987d090a87616e208634fd041
nadhdezz011-alt <nadhdezz011@gmail.com>
2025-10-10 12:10:08 +0100

    Addbutton 2

--- Assets/Scripts/LeanTweenEjemplo.cs
+++ Assets/Scripts/LeanTweenEjemplo.cs
@@ -1,25 +1,42 @@
-using UnityEngine;
+using System;
 using TMPro;
+using UnityEngine;
 public class LeanTweenEjemplo : MonoBehaviour
 {
     [SerializeField]
     GameObject objeto;
     [SerializeField]
-    float animationtime = 0.75f;
+    float animationTime = 0.75f;
     [SerializeField]
     Vector3 newPosition = new Vector3(0.0f, 4.10f, 0.0f);
     [SerializeField]
     LeanTweenType curva;
-   
+    Vector3 originalScale;
+
+    [SerializeField]
+    TextMeshProUGUI etiquetaTexto;
+    private void Start()
+    {
+        originalScale = objeto.transform.localScale;
+        etiquetaTexto.text = "Agrandar";
+    }
     public void AccionBoton()
     {
-        LeanTween.move(objeto, Vector3.zero, 0.0f);
-        LeanTween.move(objeto, newPosition, animationtime).setEase(curva);
-    }
+        if (LeanTween.isTweening(objeto) == false)
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            if (objeto.transform.localScale == originalScale)
+            {
+                LeanTween.scale(objeto, originalScale * 3f, animationTime).setEase(curva);
+                etiquetaTexto.text = "Encoger";
+            }  
+            else
+            {
+               LeanTween.scale(objeto, originalScale, animationTime).setEase(curva);
+                etiquetaTexto.text = "Agrandar";
+            }
+        }
+       
     }
+   
 }
